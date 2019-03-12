6f7e20d7c14d1d3ccde148e397f2da295a3bdfea
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-12 02:14:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,14 @@
 def countdown(integer)
     while integer > 0
       puts "#{integer} seconds!"
-      count-=1
+      count -= 1
     end
     return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  while integer > 0
+    integer -= 1
+    sleep(1)
+  end
 end
